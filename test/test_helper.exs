0e78e4326531992119d6be17a38a2b7dd0014e39
Malte Krupa <github-nqyroppg@nafn.de>
2022-07-17 20:04:54 +0200

    Enable auto-screenshots on failure and upload
    as artifacts.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,5 @@
 {:ok, _} = Application.ensure_all_started(:wallaby)
 Application.put_env(:wallaby, :base_url, WallabyTestGithubActionsWeb.Endpoint.url())
+Application.put_env(:wallaby, :screenshot_on_failure, true)
 
 ExUnit.start()
